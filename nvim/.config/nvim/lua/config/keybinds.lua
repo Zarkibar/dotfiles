5bc8ae20813ccd486831c5ea0011332271c48fc7
Zarkibar <bgeuser30@gmail.com>
2026-05-13 00:32:30 +0600

    Used josean-dev's config instead cuz I'm lazy

--- nvim/.config/nvim/lua/config/keybinds.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " "
-
-local keymap = vim.keymap
-
--- keymap.set("n", "<leader>n", vim.cmd.Ex)
-
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlight" })
-
-
