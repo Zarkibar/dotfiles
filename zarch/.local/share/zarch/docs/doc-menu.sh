b3169ed1e373f0cdc7111963543b1961f6e0e763
Zarkibar <bgeuser30@gmail.com>
2026-06-22 23:20:25 +0600

    uwsm package worked

--- zarch/.local/share/zarch/docs/doc-menu.sh
+++ zarch/.local/share/zarch/docs/doc-menu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOCS="Github
+Hyprland
 DOC 1
 DOC 2"
 
@@ -8,7 +9,10 @@ chosen=$(echo "$DOCS" | rofi -dmenu --prompt "Docs")
 
 case "$chosen" in
   "Github")
-    kitty -e mdp "$HOME/.local/share/zarch/docs/github-setup.md"
+    kitty -e glow -p "$HOME/.local/share/zarch/docs/github-setup.md"
+    ;;
+  "Hyprland")
+    kitty -e glow -p "$HOME/.local/share/zarch/docs/hyprland.md"
     ;;
   "DOC 1")
     notify-send "DOC 1" "You have selected doc 1 from documentation"
